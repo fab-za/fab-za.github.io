f672c014af6e85524aa91f7c14357cc71efc2638
Ana Zainal <tarzanazainal@gmail.com>
2020-10-28 02:15:59 +0000

    added to merry gizmomas page

--- js/project.js
+++ js/project.js
@@ -65,6 +65,23 @@ function plusSlides(n,no){
     slides[slideIndex[no]-1].style.display = "block";
   }
 
+
+// collapsibles
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("c_active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    } 
+  });
+}
+
 //   scroll reveal
 window.sr = ScrollReveal();
 
